c0b7bb6e84b174d2c65c3975bf552fc1da5aeeeb
Marco de Oliveira <oliveira.demarco@gmail.com>
2022-05-16 22:01:15 -0300

    Ignorar arquivo

---
+++ Fase1/Semana2/1-JavaScript_Basico/2-Numeros_e_operadores/operadores.js
@@ -0,0 +1,20 @@
+/* var operador = 100;
+var operando = 50;
+
+var resultado = operador * operando
+
+console.log(resultado) */
+
+/* var nome = "JC";
+var sobrenome = "Bombardelli";
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto); */
+
+var primeiroValor = 10;
+var segundoValor = '3';
+
+var resultado = primeiroValor - segundoValor;
+
+console.log(resultado);
